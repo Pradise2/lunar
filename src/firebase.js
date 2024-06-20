ccc7a745f6249a65c3f66fd417ee9229676a26b0
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-06-20 17:50:41 +0100

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
     apiKey: "AIzaSyAURFbyDHkq626UusPHMijpxmcUOOl5-Tw",
     authDomain: "test-f326f.firebaseapp.com",
-    databaseURL: "https://test-f326f-default-rtdb.firebaseio.com",
     projectId: "test-f326f",
     storageBucket: "test-f326f.appspot.com",
     messagingSenderId: "626801402709",
     appId: "1:626801402709:web:d3653b964333a0de6845dc",
-    measurementId: "G-517PH4LM9K"
-};
+    measurementId: "G-517PH4LM9K",
+    databaseURL: "https://test-f326f.firestore.nam5.firebasedatabase.app" 
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);

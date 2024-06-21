27e3ec1069dd2a1d4332792ada13897df0456a37
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-06-21 13:27:50 +0100

    Initial commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,14 +3,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBoapqfEEJwsZiStOWhZeTZlpFStKFCY80",
+  apiKey: "AIzaSyBoapqfEEJwsZiStOWhZeTZlpFStKFCY80",
+  databaseURL: "https://lunar-2ac46.firestore.europe-west3.firebasedatabase.app",
   authDomain: "lunar-2ac46.firebaseapp.com",
   projectId: "lunar-2ac46",
   storageBucket: "lunar-2ac46.appspot.com",
   messagingSenderId: "954289049346",
   appId: "1:954289049346:web:1a08d54b3ae4122c82fc1b",
-  measurementId: "G-57Q2844SHQ",
-    databaseURL: "https://lunar-2ac46.firestore.europe-west3.firebasedatabase.app" 
+  measurementId: "G-57Q2844SHQ"
   };
 
 

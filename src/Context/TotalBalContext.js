d0e52dc0a248330d385f98c81b504b223a0338ad
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-06-22 08:11:12 +0100

    Initial commit

--- src/Context/TotalBalContext.js
+++ src/Context/TotalBalContext.js
@@ -1,7 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import { useGlobalState } from './GlobalStateContext'; // Ensure this path is correct
-import { db } from '../firebaseConfig';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import React, { createContext, useContext, useState } from 'react';
 
 const TotalBalContext = createContext();
 
@@ -10,29 +7,10 @@ export const useTotalBal = () => {
 };
 
 export const TotalBalProvider = ({ children }) => {
-  const { globalState, updateUserData } = useGlobalState();
-  const [totalBal, setTotalBal] = useState(globalState.totalBal || 0);
+  const [totalBal, setTotalBal] = useState(0);
 
-  useEffect(() => {
-    setTotalBal(globalState.totalBal);
-  }, [globalState.totalBal]);
-
-  const addTotalBal = async (amount) => {
-    const newTotalBal = totalBal + amount;
-    setTotalBal(newTotalBal);
-    updateUserData({ totalBal: newTotalBal });
-
-    // Update in database
-    const userId = globalState.userId;
-    if (userId) {
-      try {
-        const userDocRef = doc(db, 'Game', String(userId));
-        await setDoc(userDocRef, { totalBal: newTotalBal }, { merge: true });
-      } catch (error) {
-        alert('Error saving total balance to Firestore: ' + error.message);
-        console.log('Error saving total balance:', error);
-      }
-    }
+  const addTotalBal = (amount) => {
+    setTotalBal((prevTotalBal) => prevTotalBal + amount);
   };
 
   return (

1adf3fd8e3ab1a32aae2f007cd600804845b166f
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-06-27 07:10:18 +0100

    Initial commit

--- src/Pages/Squad.js
+++ src/Pages/Squad.js
@@ -95,7 +95,7 @@ const Squad = () => {
             </button>
         </div>
     </div>
-    <footer className="w-full max-w-md flex justify-around mt-4  bg-zinc-800 py-2">
+    <footer className="w-full max-w-md flex justify-around fixed bottom-0 left-0  bg-zinc-800 py-2">
         <Footer />
     </footer>
     </body>

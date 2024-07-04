f970ab3e62b4ba2430bb9a45c056e1755763165a
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-07-04 17:26:30 +0100

    Initial commit

--- src/Pages/Farm.js
+++ src/Pages/Farm.js
@@ -1,24 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Footer from '../Component/Footer';
-import { saveProgress, getProgress } from '../firebaseConfig';
+import FormattedTime from '../Component/FormattedTime';
 import { useTotalBal } from '../Context/TotalBalContext';
+import { saveProgress, getProgress } from '../firebaseConfig';
+import './Farm.css'; 
 
-const Squad = () => {
+const Farm = () => {
   const defaultData = {
-    referralCount: 0,
-    referralEarnings: 0,
-    totalBalance: 0
+    lastActiveFarmTime: Math.floor(Date.now() / 1000),
+    farm: 0,
+    farmTime: 60,
+    isFarmActive: false,
+    claimed: false,
   };
 
-  const [copied, setCopied] = useState(false);
-  const [referralCount, setReferralCount] = useState(defaultData.referralCount);
-  const [referralEarnings, setReferralEarnings] = useState(defaultData.referralEarnings);
-  const [loading, setLoading] = useState(true);
-  const [totalBalance, setTotalBalance] = useState(defaultData.totalBalance);
-  const [buttonColor, setButtonColor] = useState('bg-zinc-700'); // State to manage button color
-  const [buttonText, setButtonText] = useState('Claim'); // State to manage button text
-  const [userId, setUserId] = useState(null); // Added userId state
-  const [userName, setUserName] = useState(null); // Added userName state
+  const [farmTime, setFarmTime] = useState(defaultData.farmTime);
+  const [farm, setFarm] = useState(defaultData.farm);
+  const [isFarmActive, setIsFarmActive] = useState(defaultData.isFarmActive);
+  const [claimed, setClaimed] = useState(defaultData.claimed);
+  const [buttonText, setButtonText] = useState('Start');
+  const farmIntervalRef = useRef(null);
+  const { addTotalBal, totalBal } = useTotalBal(); // Assuming useTotalBal provides totalBal as well
+  const [lastActiveFarmTime, setLastActiveFarmTime] = useState(defaultData.lastActiveFarmTime);
+  const [userId, setUserId] = useState(null);
+  const [userName, setUserName] = useState(null);
 
   window.Telegram.WebApp.expand();
 
@@ -37,46 +42,64 @@ const Squad = () => {
   }, []);
 
   useEffect(() => {
-    if (userId) {
-      const fetchData = async () => {
-        try {
-          const userData = await getProgress(userId);
-          console.log('User data from Firestore:', userData);
-          setReferralCount(userData.referralCount || defaultData.referralCount);
-          setReferralEarnings(userData.referralEarnings || defaultData.referralEarnings);    
-          setTotalBalance(userData.totalBalance || defaultData.totalBalance);
-          setLoading(false);
-        } catch (error) {
-          console.error('Error fetching user data:', error);
-          setLoading(false);
+    const fetchData = async () => {
+      try {
+        const userData = await getProgress(userId);
+        if (userData) {
+          const currentTime = Math.floor(Date.now() / 1000);
+          const elapsed = currentTime - userData.lastActiveFarmTime;
+
+          setLastActiveFarmTime(currentTime);
+          setClaimed(userData.claimed);
+
+          if (userData.isFarmActive) {
+            const remainingFarmTime = userData.farmTime - elapsed;
+            if (remainingFarmTime > 0) {
+              setFarm(userData.farm + elapsed * 0.01);
+              setFarmTime(remainingFarmTime);
+              setIsFarmActive(true);
+              startFarmInterval();
+              setButtonText('Farming...');
+            } else {
+              setFarm(userData.farm + userData.farmTime * 0.01);
+              setFarmTime(0);
+              setIsFarmActive(false);
+              setButtonText('Claim');
+            }
+          } else {
+            setFarm(userData.farm);
+            setFarmTime(userData.farmTime);
+            setIsFarmActive(false);
+            setButtonText('Start');
+          }
         }
-      };
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    if (userId) {
       fetchData();
     }
   }, [userId]);
 
-  useEffect(() => {
-    if (referralEarnings > 0) {
-      setButtonColor('bg-purple-700'); // Change button color to purple if referralEarnings > 0
-    } else {
-      setButtonColor('bg-zinc-700'); // Change button color back to zinc if referralEarnings is 0
+  const saveData = async (additionalData = {}) => {
+    try {
+      await saveProgress(userId, {
+        lastActiveFarmTime: Math.floor(Date.now() / 1000),
+        farm,
+        farmTime,
+        isFarmActive,
+        claimed,
+        totalBal,
+        ...additionalData, // Spread additionalData to include any other data needed
+      });
+    } catch (error) {
+      console.error('Error saving user data:', error);
     }
-  }, [referralEarnings]);
+  };
 
   useEffect(() => {
-    const saveData = async () => {
-      try {
-        await saveProgress(userId, {
-          referralCount,
-          referralEarnings, 
-          totalBalance,
-        });
-        console.log('User data saved successfully');
-      } catch (error) {
-        console.error('Error saving user data:', error);
-      }
-    };
-
     const handleBeforeUnload = (e) => {
       if (userId) {
         saveData();
@@ -90,93 +113,106 @@ const Squad = () => {
       window.removeEventListener('beforeunload', handleBeforeUnload);
       if (userId) {
         saveData();
-        console.log('Saving user data on cleanup');
       }
     };
-  }, [userId, referralCount, referralEarnings, totalBalance]);
+  }, [userId, farm, farmTime, isFarmActive, claimed, totalBal]);
 
-  const handleClaim = () => {
-    console.log('Claim button clicked');
-    setButtonColor('bg-zinc-700'); // Change the button color back to zinc after clicking
-    setButtonText('Claimed'); // Change the button text to "Claimed"
-    setTotalBalance((prevTotalBalance) => prevTotalBalance + referralEarnings); // Add referralEarnings to totalBalance
+  const startFarmInterval = () => {
+    if (farmIntervalRef.current) return;
+    farmIntervalRef.current = setInterval(() => {
+      setFarmTime((prevFarmTime) => {
+        if (prevFarmTime <= 0) {
+          clearInterval(farmIntervalRef.current);
+          farmIntervalRef.current = null;
+          setIsFarmActive(false);
+          setButtonText('Claim');
+          return 0;
+        } else {
+          setFarm((prevFarm) => prevFarm + 0.01);
+          return prevFarmTime - 1;
+        }
+      });
+    }, 1000);
   };
 
-  const copyToClipboard = () => {
-    const reflink = `https://t.me/yourcoinhot_bot?start=ref_${userId}`;
+  const startFarm = () => {
+    setIsFarmActive(true);
+    setLastActiveFarmTime(Math.floor(Date.now() / 1000));
+    startFarmInterval();
+    setClaimed(false);
+    setButtonText('Farming...');
+  };
 
-    if (navigator.clipboard && navigator.clipboard.writeText) {
-      navigator.clipboard.writeText(reflink).then(() => {
-        setCopied(true);
-        setTimeout(() => setCopied(false), 1000);
-      }).catch(err => {
-        console.error('Failed to copy text:', err);
-      });
-    } else {
-      const textArea = document.createElement('textarea');
-      textArea.value = reflink;
-      document.body.appendChild(textArea);
-      textArea.select();
-      try {
-        document.execCommand('copy');
-        setCopied(true);
-        setTimeout(() => setCopied(false), 2000);
-      } catch (err) {
-        console.error('Failed to copy:', err);
-      }
-      document.body.removeChild(textArea);
+  const claimFarmRewards = async () => {
+    const claimedAmount = farm;
+    const newTotalBal = totalBal + claimedAmount;
+    
+    // Update the balance immediately
+    addTotalBal(claimedAmount); 
+    setFarm(0);
+    setFarmTime(60);
+    setClaimed(true);
+    setButtonText('Start');
+    
+    // Persist data immediately after claiming
+    await saveData({ totalBal: newTotalBal, claimed: true }); 
+  };
+  
+
+  const handleButtonClick = () => {
+    if (buttonText === 'Start') {
+      startFarm();
+    } else if (buttonText === 'Claim') {
+      claimFarmRewards();
     }
   };
 
   return (
-    <div className="min-h-screen bg-zinc-900 text-white flex flex-col justify-between bg-cover bg-center">
-      <div className="flex-grow flex flex-col items-center p-6">
-        <h1 className="text-center text-2xl font-bold">
-          The bigger the tribe, the better the vibe!
-        </h1>
-        {!loading && (
-          <>
-            <div className="w-full max-w-md bg-zinc-800 rounded-lg p-4 mb-4">
-              <p className="text-center text-zinc-400">Total Reward</p>
-              <p className="text-center text-3xl font-bold">
-                {totalBalance.toFixed(2)} <span className="text-purple-400"></span>
-              </p>
-            </div>
-            <div className="w-full max-w-md bg-zinc-800 rounded-lg p-4 mb-4">
-              <p className="text-center text-zinc-400">Your rewards</p>
-              <p className="text-center text-3xl font-bold">
-                {referralEarnings.toFixed(2)} <span className="text-purple-400"></span>
-              </p>
-              <p className="text-center text-zinc-400 mb-4">1000 per friend's refer</p>
-              <button 
-                className={`w-full py-2 rounded-lg ${buttonColor}`} 
-                onClick={handleClaim}>
-                {buttonText}
-              </button>
-            </div>
-            <div className="w-full max-w-md bg-zinc-800 rounded-lg p-4 mb-4 flex justify-between items-center">
-              <div className="flex items-center space-x-2">
-                <span className="material-icons text-zinc-400">group</span>
-                <p>Your team</p>
-              </div>
-              <p>{referralCount} Users</p>
-            </div>
-          </>
-        )}
-        <div className="w-full max-w-md flex space-x-2 mt-5">
-          <button className="flex-1 bg-gradient-to-r from-purple-800 to-indigo-800 py-2 rounded-lg" onClick={copyToClipboard}>
-            Invite friends
-          </button>
-          <button className="bg-zinc-700 p-2 rounded-lg" onClick={copyToClipboard}>
-            {copied ? <span>Copied!</span> : <span>Copy</span>}
-          </button>
+ 
+    <body class="min-h-screen bg-zinc-900 text-white flex flex-col justify-between">
+    <div className="flex-grow flex flex-col items-center justify-start px-4 mt-8">
+        <div className="text-center">
+            <h1 className="text-3xl md:text-4xl font-bold mb-2">Lunar Token Farming</h1>
         </div>
-      </div>
-      <footer className="w-full max-w-md flex justify-around fixed bottom-0 left-0 bg-zinc-800 py-2">
-        <Footer />
-      </footer>
+    
+        <div className="mt-4">
+            <img src="avatar_url" alt="Avatar" className="w-16 h-16 rounded-full mx-auto"/>
+            <p className="text-center text-zinc-400 italic mt-2">{userName ? `${userName} ` : ''}</p>
+        </div>
+    
+        <div className="text-center mt-2 md:mt-4">
+            <p className="text-zinc-400">Keep your farm thriving!</p>
+        </div>
+    
+        <div className="bg-gradient-to-r from-purple-800 to-indigo-800 text-center py-3 px-6 rounded-lg flex justify-center items-center mt-5 transition transform hover:scale-105 hover:shadow-lg">
+            <span className="material-icons mr-2">access_time</span>
+            <p className="font-bold text-lg md:text-xl"><FormattedTime time={farmTime} /></p>
+        </div>
+    
+        <div className="mt-8 bg-zinc-800 rounded-lg p-6 relative w-full max-w-sm transition transform hover:scale-105 hover:shadow-lg">
+            <p className="text-center text-zinc-400">Era Reward</p>
+            <p className="text-center text-4xl font-bold mt-2">
+                {farm.toFixed(2)} <span className="text-purple-400">lunar</span>
+            </p>
+        </div>
+    
+        <button
+            className="mt-4 bg-zinc-700 text-white py-2 px-6 rounded-lg transition transform hover:bg-zinc-600 disabled:opacity-50 disabled:cursor-not-allowed"
+            onClick={handleButtonClick}
+            disabled={isFarmActive && farmTime > 0}
+        >
+            {buttonText}
+        </button>
     </div>
+
+  
+    <div className="w-full max-w-md flex justify-around mt-4 fixed bottom-0 left-0 bg-zinc-800 py-2">
+        <Footer />
+    </div>
+</body>
+
+
   );
 };
 
-export default Squad;
+export default Farm;
